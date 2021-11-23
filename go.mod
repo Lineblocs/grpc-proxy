1a6e84b15334da9e43622726fe2f9b37a37c816c
Nadir Hamid <matrix.nad@gmail.com>
2021-11-23 00:49:38 +0000

    add more logic

---
+++ go.mod
@@ -0,0 +1,30 @@
+module lineblocs.com/grpc-proxy
+
+go 1.17
+
+require (
+	bitbucket.org/infinitet3ch/lineblocs-go-helpers v0.0.0-20210723000142-7b8ed9cfaa26 // indirect
+	github.com/aws/aws-sdk-go v1.33.5 // indirect
+	github.com/clockworksoul/smudge v1.0.0 // indirect
+	github.com/go-chi/chi v4.1.2+incompatible // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.1.2 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/jinzhu/now v1.1.1 // indirect
+	github.com/jmespath/go-jmespath v0.3.0 // indirect
+	github.com/koding/websocketproxy v0.0.0-20181220232114-7ed82d81a28c // indirect
+	github.com/mailgun/mailgun-go/v4 v4.1.3 // indirect
+	github.com/mailru/easyjson v0.7.1 // indirect
+	github.com/mwitkow/grpc-proxy v0.0.0-20181017164139-0f1106ef9c76 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stripe/stripe-go/v71 v71.28.0 // indirect
+	github.com/ttacon/builder v0.0.0-20170518171403-c099f663e1c2 // indirect
+	github.com/ttacon/libphonenumber v1.1.0 // indirect
+	golang.org/x/net v0.0.0-20211118161319-6a13c67c3ce4 // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/grpc v1.42.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"strings"
+	"net"
+	"log"
+	"fmt"
+	"net/http"
+	"net/url"
+	"database/sql"
+	"github.com/mwitkow/grpc-proxy/proxy"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+		"github.com/koding/websocketproxy"
+	lineblocs "bitbucket.org/infinitet3ch/lineblocs-go-helpers"
+)
+
+var (
+	director proxy.StreamDirector
+)
+var db* sql.DB;
+
+func getAvailableMediaserver() (*lineblocs.MediaServer, error) {
+	servers, err := lineblocs.CreateMediaServers()
+
+	if err != nil {
+		return nil,err
+	}
+	return servers[0], nil
+}
+func proxyWebsocket() {
+	server, err := getAvailableMediaserver()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	splitted := strings.Split(server.PrivateIpAddress,":")
+	addr:= splitted[0]
+	backend, err := url.Parse(addr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = http.ListenAndServe(":8018", websocketproxy.NewProxy(backend))
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+func LineblocsTransparentHandler() {
+	grpc.NewServer(
+		grpc.CustomCodec(proxy.Codec()),
+		grpc.UnknownServiceHandler(proxy.TransparentHandler(LineblocsStreamDirector)))
+}
+
+// Provide sa simple example of a director that shields internal services and dials a staging or production backend.
+// This is a *very naive* implementation that creates a new connection on every request. Consider using pooling.
+func LineblocsStreamDirector(ctx context.Context, fullMethodName string) (context.Context, *grpc.ClientConn, error) {
+	// Make sure we never forward internal services.
+	fmt.Println("method is: " + fullMethodName)
+	if strings.HasPrefix(fullMethodName, "/com.example.internal.") {
+		return nil, nil, grpc.Errorf(codes.Unimplemented, "Unknown method")
+	}
+	md, ok := metadata.FromIncomingContext(ctx)
+	// Copy the inbound metadata explicitly.
+	outCtx, _ := context.WithCancel(ctx)
+	outCtx = metadata.NewOutgoingContext(outCtx, md.Copy())
+	fmt.Println("created out context...")
+	if ok {
+		// Decide on which backend to dial
+		fmt.Println("ROUTING NOW...")
+		val, exists := md[":authority"]
+		if !exists {
+			fmt.Println("no authority found..\r\n");
+			return nil, nil, grpc.Errorf(codes.Unimplemented, "no authority found")
+
+		}
+		authority := val[0]
+		fmt.Println("authority IP: " + authority)
+
+		server, err := getAvailableMediaserver()
+		if err != nil {
+			return nil, nil, err
+		}
+		splitted := strings.Split(server.PrivateIpAddress,":")
+		ip:= splitted[0]+":9000"
+
+		fmt.Println("routing to server: " + ip)
+		conn, err := grpc.DialContext(ctx, ip, grpc.WithCodec(proxy.Codec()))
+		return outCtx, conn, err
+	}
+	return nil, nil, grpc.Errorf(codes.Unimplemented, "Unknown method")
+}
+
+func main() {
+	var err error
+	fmt.Println("starting server..")
+	db, err =lineblocs.CreateDBConn()
+	if err != nil {
+		panic(err)
+	}
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", 9001))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	fmt.Println("started listener")
+	//grpcServer := grpc.NewServer()
+	//grpcServer := grpc.NewServer(grpc.CustomCodec(proxy.Codec()))
+	director := LineblocsStreamDirector
+	grpcServer := grpc.NewServer(grpc.CustomCodec(proxy.Codec()))
+	proxy.RegisterService(grpcServer, director,
+		"grpc.Lineblocs",
+		"createBridge", "Ping", "PingError", "PingList")
+ 	//LineblocsRegisterService(grpcServer)
+	//RegisterLineblocsServer(grpcServer, s)
+
+	go proxyWebsocket()
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
